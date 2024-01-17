fa836c182cbc4984bda375b9b7e128ff1b41c314
Mir1a <miriamotuss@gmail.com>
2024-01-17 19:12:23 +0200

    update models

--- midas/admin.py
+++ midas/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 
-from .models import user
-from .models import transaction
-from .models import item
-from .models import order
-from .models import supplies
+from .models import User
+from .models import Transaction
+from .models import Item
+from .models import Order
+from .models import Supply
 # Register your models here.
-admin.site.register(user)
-admin.site.register(transaction)
-admin.site.register(item)
-admin.site.register(order)
-admin.site.register(supplies)
+admin.site.register(User)
+admin.site.register(Transaction)
+admin.site.register(Item)
+admin.site.register(Order)
+admin.site.register(Supply)
--- midas/models.py
+++ midas/models.py
@@ -1,13 +1,38 @@
 from django.db import models
-
+from django.utils import timezone
+from . import choices
+from django.core import validators
 # Create your models here.
 
-class item(models.Model):
-    id = models.AutoField(primary_key=True)
-    title = models.TextField(max_length=500)
-    description = models.TextField(max_length=1000, null=True)
+class User(models.Model):
+    email = models.EmailField(max_length=255, unique=True, null=False, blank=False)
+    name = models.CharField(max_length=255, null=True, blank=True)
+    last_name = models.CharField(max_length=255, null=True, blank=True)
+    password = models.CharField(max_length=255, null=False, blank=False)
+    avatar = models.ImageField(upload_to='users/avatar', null=True, blank=True)
+    born = models.DateField(default=timezone.now, null=True, blank=True)
+    favorites = models.ManyToManyField(to="midas.Item")
+
+    def __str__(self):
+        return self.id
+
+
+class Transaction(models.Model):
+    code = models.CharField(max_length=50)
+    user = models.ForeignKey(to="midas.User", on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    code = models.CharField(max_length=15)
+    status = models.CharField(max_length=255, choices=choices.statuses)
+    order = models.OneToOneField(to="midas.Order", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.id
+
+
+class Item(models.Model):
+    title = models.TextField(max_length=500,  null=False, blank=False)
+    description = models.TextField(max_length=1000, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[validators.MinValueValidator(1)])
+    code = models.IntegerField(unique=True, null=False, blank=False)
     color = models.CharField(max_length=20)
     weight = models.DecimalField(max_digits=10, decimal_places=1)
     height = models.DecimalField(max_digits=10, decimal_places=1)
@@ -16,46 +41,26 @@ class item(models.Model):
     def __str__(self):
         return self.title
 
-class user(models.Model):
-    models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
-    password = models.CharField(max_length=255)
-    #avatar = models.ImageField(upload_to='avatars')
-    favorite = models.ForeignKey(to=item, on_delete=models.CASCADE, null=True)
 
-    def __str__(self):
-        return self.id
-
-class order(models.Model):
-    models.AutoField(primary_key=True)
-    item = models.ForeignKey(to=item, on_delete=models.CASCADE, null=True)
-    amount_items = models.IntegerField()
-    user = models.OneToOneField(to=user, on_delete=models.CASCADE, null=True)
+class Order(models.Model):
+    # region           -----Information-----
     sum_price = models.DecimalField(max_digits=10, decimal_places=2)
-    status = models.BooleanField(default=False)
+    amount_items = models.PositiveIntegerField()
+    # TODO сделать через choices
+    status = models.IntegerField(choices=choices.order_statuses)
+    # endregion
+
+    # region           -----Relation-----
+    user = models.ForeignKey(to="midas.User", on_delete=models.CASCADE)
+    items = models.ManyToManyField(to="midas.Item")
+    # endregion
 
     def __str__(self):
         return self.id
 
-class transaction(models.Model):
-    models.AutoField(primary_key=True)
-    code = models.CharField(max_length=50)
-    user = models.ManyToManyField(to=user, null=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    status = models.BooleanField(default=False)
-    order = models.OneToOneField(to=order, on_delete=models.CASCADE, null=True)
+
+class Supply(models.Model):
+    data = models.JSONField()
 
     def __str__(self):
         return self.id
-
-
-
-class supplies(models.Model):
-    models.AutoField(primary_key=True)
-    #словарь сделать
-
-    def __str__(self):
-        return self.id
-
