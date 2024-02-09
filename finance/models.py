538abb8221b6a9bdb7ccec5056f2b246dd5ca976
dmitry.zhigiliy <dmitry.zhigiliy@accelario.com>
2024-02-09 19:47:39 +0200

    ItemViewSet add sorting and filtering

--- finance/models.py
+++ finance/models.py
@@ -8,6 +8,7 @@ class Transaction(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     status = models.CharField(max_length=255, choices=choices.statuses)
     order = models.OneToOneField(to="product.Order", on_delete=models.CASCADE)
+
     class Meta:
         verbose_name_plural = "Транзакции"
         verbose_name = "Транзакция"
