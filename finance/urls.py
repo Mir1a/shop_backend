84ab0dbb4d5da5ea1a377dae60d5be5535352c7f
Mir1a <miriamotuss@gmail.com>
2024-02-28 15:50:47 +0200

    +signals/send mail

---
+++ finance/urls.py
@@ -0,0 +1,11 @@
+# # region				-----External Imports-----
+# # endregion
+#
+# # region				-----Internal Imports-----
+# from .api import urls as urls
+# # endregion
+#
+# # region			  -----Supporting Variables-----
+# # endregion
+#
+# urlpatterns = urls.frontend_router.urls
--- midas/signals/postprocessing/signals.py
+++ midas/signals/postprocessing/signals.py
@@ -1,8 +1,22 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from ...models import User as midas_user
+from ... import models
+from django.conf import settings
+import logging
+import smtplib
+#from ....secretshop import django
+#from secretshop import mail_settings
 
-@receiver(post_save, sender=midas_user)
+# region                 -----Emails settings-----
+password = "buvloplwstlzvlph"
+email_sender = "miriamotuss@gmail.com"
+smtp_server = smtplib.SMTP("smtp.gmail.com", 587)
+smtp_server.starttls()
+smtp_server.login(email_sender, password)
+# endregion
+
+@receiver(post_save, sender=models.User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        print("hello")
+        email_getter = instance.email
+        smtp_server.sendmail(email_sender, email_getter,  "hello")
