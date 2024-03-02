a9d7b10044831f0952c97c4d6481eb40c99f2dc8
Mir1a <miriamotuss@gmail.com>
2024-03-02 15:27:21 +0200

    supply_sender serializer

--- product/api/general/serializers.py
+++ product/api/general/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from product.models import Item
 from product.models import Order
-
+from product.models import Supply_sender
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
@@ -15,4 +15,10 @@ class OrderSerializer(serializers.ModelSerializer):
         model = Order
         fields = ("id", "sum_price")
 # "amount_items", "status", "user", "items"
+
+class Supply_senderSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Supply_sender
+        fields = ("item", "amount", "code")
 
